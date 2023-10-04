b4afa8fdb62daefa5610bb32879663a17694aec1
cqzho <129482052+cqzho@users.noreply.github.com>
2023-10-04 10:32:25 +0800

    Update 温度.js

--- js/温度.js
+++ js/温度.js
@@ -1,6 +1,6 @@
 muban.mxone5.二级.desc = ';;.video-info-items:eq(2);.video-info-items:eq(1);.video-info-items:eq(0)';
 var rule = Object.assign(muban.mxone5,{
-    title:'Vid影视',
+    title:'Vid影视https://vidhub.link/',
     host:'https://vidhub.tv',
     url:'/vodshow/fyclass--------fypage---.html',
     searchUrl:'/vodsearch/**----------fypage---.html',
